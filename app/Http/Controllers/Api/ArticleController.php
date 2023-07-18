a2594dab3b97cf63c2c1c7342a8bcff61ad4871a
Carlos Andres Pacheco Fuentes <carlosandres4020@gmail.com>
2023-07-17 21:15:45 -0500

    Se crea la clase ArticleResource.php para elimiar el codigo del contralador y apoyarse en la clase que se creo

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
@@ -11,21 +12,6 @@ class ArticleController extends Controller
 
     public function show(Article $article)
     {
-        return response()->json(
-            [
-                'data' =>[
-                    'type' => 'articles',
-                    'id' => (string) $article->getRouteKey(),
-                    'attributes' => [
-                        'title' => $article->title,
-                        'slug' => $article->slug,
-                        'content' => $article->content
-                    ],
-                    'links' => [
-                        'self' => route('articles.show', $article)
-                    ],
-                ]
-            ]
-        );
+        return ArticleResource::make($article);
     }
 }
---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+                'type' => 'articles',
+                'id' => (string) $this->resource->getRouteKey(),
+                'attributes' => [
+                    'title' => $this->resource->title,
+                    'slug' => $this->resource->slug,
+                    'content' => $this->resource->content
+                ],
+                'links' => [
+                    'self' => route('articles.show', $this->resource)
+                ],
+            ];
+    }
+}
